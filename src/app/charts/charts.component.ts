6f0bb7805189afeea6ad7dadda66df909b0f00af
dinsan <dinmylk@gmail.com>
2019-07-11 01:15:42 +0200

    the chert url updayted

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import * as Chart from 'chart.js'
 import { ChartsService } from '../share/charts.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-charts',
@@ -14,111 +15,53 @@ export class ChartsComponent implements OnInit {
   public refChart: ElementRef;
 
   public chartData: any;
+  public data; 
 
-  public constructor(private chartsService : ChartsService) { 
+  public constructor(private chartsService : ChartsService, private http: HttpClient) { 
     this.chartData = {};
-    
+
   }
 
-  public ngOnInit() { 
-    //console.log(this.chartsService.getByCountryList())
-    this.chartsService.getByCountryList();
 
-    this.chartData = {
-      labels: ["es",
-      "en",
-      "nl",
-      "it",
-      "de",
-      "fr",
-      "ru",
-      "pt",
-      "pl",
-      "ca",
-      "zh",
-      "da",
-      "ko",
-      "zh-cn",
-      "ro",
-      "ja",
-      "lt",
-      "nb",
-      "sk",
-      "sl",
-      "zh-hk"],
-      datasets: [{
-          label: 'By country',
-          data: [365,
-            278,
-            194,
-            141,
-            60,
-            58,
-            25,
-            21,
-            12,
-            8,
-            7,
-            3,
-            3,
-            3,
-            2,
-            1,
-            1,
-            1,
-            1,
-            1,
-            1],
-          backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-      }]
-    };
+ 
+
+  public ngOnInit() { 
   }
 
   public ngAfterViewInit() { 
-    let chart = this.refChart.nativeElement;
-    let ctx = chart.getContext("2d");
-    let myChart = new Chart(ctx, {
-        type: 'polarArea',
-        data: this.chartData,
-        options: {
-          tooltips: {
-            enabled: false
-       },
-                plugins: {
-               datalabels: {
-                   formatter: (value, ctx) => {
+    this.http.get('http://localhost:8000/bycountry').subscribe((res: any[]) => { 
+      let chart = this.refChart.nativeElement;
+      let ctx = chart.getContext("2d");
+      let myChart = new Chart(ctx, {
+          type: 'polarArea',
+          data: res["ByCountry"],
+          options: {
+            tooltips: {
+              enabled: false
+         },
+                  plugins: {
+                 datalabels: {
+                     formatter: (value, ctx) => {
+                     
+                       let sum = 0;
+                       let dataArr = ctx.chart.data.datasets[0].data;
+                       dataArr.map(data => {
+                           sum += data;
+                       });
+                       let percentage = (value*100 / sum).toFixed(2)+"%";
+                       return percentage;
+     
                    
-                     let sum = 0;
-                     let dataArr = ctx.chart.data.datasets[0].data;
-                     dataArr.map(data => {
-                         sum += data;
-                     });
-                     let percentage = (value*100 / sum).toFixed(2)+"%";
-                     return percentage;
-   
-                 
-                   },
-                   color: '#fff',
-                        }
-           }
-        }
+                     },
+                     color: '#fff',
+                          }
+             }
+          }
+      });
+
     });
+        
+  
   }
 
 }
--- src/app/share/charts.service.ts
+++ src/app/share/charts.service.ts
@@ -2,30 +2,38 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 
-
-
 @Injectable({
   providedIn: 'root'
 })
 export class ChartsService {
-
-  constructor(private http: HttpClient,) { 
-
+  constructor(private http: HttpClient) { 
+  
   }
 
-   private  ByCountry = [
-    {"ByCountry": {"Label": ["es", "en", "nl", "it", "de", "fr", "ru", "pt", "pl", "ca", "zh", "da", "ko", "zh-cn", "ro", "ja", "lt", "nb", "sk", "sl", "zh-hk"], "Data": [365, 278, 194, 141, 60, 58, 25, 21, 12, 8, 7, 3, 3, 3, 2, 1, 1, 1, 1, 1, 1]}}
-  ];
 
-  getByCountryList() {  
+
+   getByCountryList() {  
   
+    var chartarray = [];
     // this is reading from URL
     //----------------------------
-     this.http.get('hhttp://127.0.0.1:8000/bycountry').subscribe((res: any[]) => {
-     console.log(res);
-     
-      return this.ByCountry;
+     this.http.get('http://localhost:8000/bycountry').subscribe((res: any[]) => {
+       return res;
     });
   
+
   } 
+
+  private data = [];  
+
+   getOption() {  
+
+    this.http.get('http://localhost:8000/bycountry').subscribe((res: any[]) => {
+      
+      this.data.push(res);
+      return this.data;  
+   });
+     
+   } 
+
 }
